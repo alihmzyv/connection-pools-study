1edad464e795df557f37387ce5daab1af51d9ef5
Ali Hamzayev <alihmzyv@gmail.com>
2024-03-31 14:40:27 +0400

    Initial commit

---
+++ src/main/java/az/ingress/connectionpoolsstudy/ConnectionPoolsStudyApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.connectionpoolsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionPoolsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionPoolsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/connectionpoolsstudy/ConnectionPoolsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.connectionpoolsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionPoolsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
